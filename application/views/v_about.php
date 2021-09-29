c4b9223231744e45f0d10630cad4b44fdacda9fd
qwangseng <qwang9664@gmail.com>
2021-09-29 20:26:13 +0700

    membuat template sederhana yang dinamis

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Gabriel</li>
+        <li>Nama Belakang : Sengkuang</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan kesehatan Gg.sumber agung 2</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pontianak</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>streetball</li>
+        <li>game</li>
+    </ul>
+</section>
